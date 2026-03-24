1f23596f98f7626a96372bfcc5c220758d013586
arnau-avson <arnau@avson.eu>
2026-03-24 10:02:40 +0100

    fix: update preview and server ports to 3002 for consistency

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: 3002,
+    port: 3000,
     allowedHosts: ['avson.eu'],
   },
   preview: {
     host: '0.0.0.0',
-    port: 3002,
+    port: 3000,
     allowedHosts: ['avson.eu'],
   },
   build: {
