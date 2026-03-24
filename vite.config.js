1d8bc3d745c1560576441abd990530ed1a9bf7a3
arnau-avson <arnau@avson.eu>
2026-03-24 09:51:07 +0100

    feat: update Vite configuration to enhance server and build settings

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,25 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 3002,
+    allowedHosts: ['avson.eu'],
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3002,
+    allowedHosts: ['avson.eu'],
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
